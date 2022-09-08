7d169058069f2df386c032099adc5d252d7bc4aa
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 15:56:59 -0700

    commit 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ ** main - prints string with printf
+ ** Return: 0 throw when success
+ **/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
