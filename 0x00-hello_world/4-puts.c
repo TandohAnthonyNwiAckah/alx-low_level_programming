fc7a610c319bf00cddae764ee1f7111847b91c7c
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 15:41:39 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-/*
- *main  print String with puts
- *return 0
- *
- */
-int main(void){
-
-
+/**
+ ** main - print string with puts fxn
+ ** Return: throw  0 for success
+ **
+ **/
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 
-return 0 ;
+return (0);
 }
-
-
 
