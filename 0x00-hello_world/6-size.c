4a92bdc2d5d8ec423332965928208980d156a3c7
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 16:52:02 -0700

    commit 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-** main - prints the size of types
-** Return: 0 throw success
-**/
+ ** main - prints the size of types
+ ** Return: 0 throw success
+ **/
 int main(void)
 {
-printf(“The size of a char is : %d  byte(s)\n", sizeof(char));
-printf(“The size of an int is : %d  byte(s)\n", sizeof(int));
-printf(“The size of a long int is :  %d  byte(s)\n", sizeof(long int));
-printf(“size of a long long int :  %d  byte(s)\n", sizeof(long long int));
-printf(“The size of a float is:  %d  byte(s)\n", sizeof(float));
+printf("The size of a chat is : %d byte(s)\n", sizeof(char));
+printf("The size of an iny is : %d byte(s)\n", sizeof(int));
+printf("The size of a long iny is %d byte(s)", sizeof(long int));
+printf("Size of a long long  int is: %d bytes(s)\n", sizeof(long long int));
+printf("The size of a float is %d bytes(s)\n", sizeof(float));
 return (0);
-}
