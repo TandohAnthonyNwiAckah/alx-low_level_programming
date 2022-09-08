fe5f5f010df3931749a2821e1b38bedbf4ffe94b
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 15:23:52 -0700

    commit 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ *main  print String with puts
+ *return 0
+ *
+ */
+int main(void){
+
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return 0 ;
+}
+
+
+
