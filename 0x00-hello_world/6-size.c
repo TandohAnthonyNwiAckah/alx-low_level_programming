f88754601df60a20e9289f5d18fa50e18b385a10
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 17:59:10 -0700

    commit 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- ** main - prints the size of types
- ** Return: 0 throw success
- **/
+** main - prints the size of types
+** Return: 0 throw success
+**/
 int main(void)
 {
 int myInt;
@@ -10,11 +10,10 @@ long int myLongInt;
 long long int myLInt;
 char myChar;
 float myFloat;
-
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(myChar));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)size(myInt));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(myLongInt));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(myLInt));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(myFloat));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(myInt));
+printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(myLongInt));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(myLInt));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(myFloat));
 return (0);
 }
