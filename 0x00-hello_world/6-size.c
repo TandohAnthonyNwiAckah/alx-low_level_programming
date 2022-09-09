426bdfff8144af7b78440e3524b6ebc511e1c386
Anthony <anthony.tandoh@tanacom.io>
2022-09-08 17:52:47 -0700

    commit 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float myFloat;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(myChar));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)size(myInt));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(mylongInt));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(myLongInt));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(myLInt));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(myFloat));
 return (0);
