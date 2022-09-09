3686f661c63766c0dbd419bc32390bb833058a5b
Anthony <anthony.tandoh@tanacom.io>
2022-09-09 02:58:26 -0700

    commit fix  6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
 **/
 int main(void)
 {
-int myInt;
-long int myLongInt;
-long long int myLInt;
-char myChar;
-float myFloat;
-printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
-printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
-printf("Size of long int: %lu byte(s)\n", sizeof(myLongInt));
-printf("Size of long long int: %lu byte(s)\n", sizeof(myLInt));
-printf("Size of float: %lu byte(s)\n", sizeof(myFloat));
+int d;
+long int g;
+long long int t;
+char r;
+float p;
+printf("Size of a char: %lu byte(s)\n", sizeof(d));
+printf("Size of an int: %lu byte(s)\n", sizeof(t));
+printf("Size of long int: %lu byte(s)\n", sizeof(g));
+printf("Size of long long int: %lu byte(s)\n", sizeof(r));
+printf("Size of float: %lu byte(s)\n", sizeof(p));
 return (0);
 }
