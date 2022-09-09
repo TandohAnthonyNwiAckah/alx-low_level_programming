50968f4bb137f3d2fc4cf5c1ac118a4cabffe629
Anthony <anthony.tandoh@tanacom.io>
2022-09-09 03:15:56 -0700

    commit fixed 6-size.c error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ long int g;
 long long int t;
 char r;
 float p;
-printf("Size of a char: %lu byte(s)\n", sizeof(d));
-printf("Size of an int: %lu byte(s)\n", sizeof(t));
+printf("Size of a char: %lu byte(s)\n", sizeof(r));
+printf("Size of an int: %lu byte(s)\n", sizeof(d));
 printf("Size of long int: %lu byte(s)\n", sizeof(g));
-printf("Size of long long int: %lu byte(s)\n", sizeof(r));
+printf("Size of long long int: %lu byte(s)\n", sizeof(t));
 printf("Size of float: %lu byte(s)\n", sizeof(p));
 return (0);
 }
