ab37c62404beac082e8edd5384ecfa5acaa8161b
Anthony <anthony.tandoh@tanacom.io>
2022-09-14 22:46:04 -0700

    commit 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - Adds to numbers and retuns the sum
+* @x: First number
+* @y: Second number
+* Return: The sum of x and y
+*/
+
+int add(int x, int y)
+{
+	int total = x + y;
+
+	return (total);
+}
