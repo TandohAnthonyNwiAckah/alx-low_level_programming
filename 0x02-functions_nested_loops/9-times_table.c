85e6ae2a6d0328a36c9a367f60287cd5f0ec04a5
Anthony <anthony.tandoh@tanacom.io>
2022-09-14 22:41:02 -0700

    commit 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* times_table - Prints the 9 times table
+* Return: void
+*/
+void times_table(void)
+{
+	int a, b;
+	int result;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			result = b * a;
+			if (b == 0)
+				_putchar(result + '0');
+			if (result < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
