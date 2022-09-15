77e17ce46276acc71a49f3ae01ad02f197204e15
Anthony <anthony.tandoh@tanacom.io>
2022-09-14 22:15:25 -0700

    commit 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints the last digit of a number
+* @n: Parameter for check
+* Return: value of the last digit
+*/
+int print_last_digit(int n)
+{
+	int ld;
+
+	if (n < 0)
+	{
+		ld = (-1 * (n % 10));
+		_putchar (ld + '0');
+		return (ld);
+	}
+
+	else
+	{
+		ld = (n % 10);
+		_putchar (ld + '0');
+		return (ld);
+	}
+
