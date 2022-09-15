7b7aa961e01324414d3f588c56a3907e95418c52
Anthony <anthony.tandoh@tanacom.io>
2022-09-14 22:55:22 -0700

    commit 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+* print_times_table - Print times table of the number given
+* @n: Parameter given
+* Returns: void
+*/
+void print_times_table(int n)
+{
+	int a, b, result;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				result = b * a;
+				if (b == 0)
+					_putchar(result + '0');
+				else if (result < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
