0f913c2cad71345c25c9ac5644d983ac806bca2a
Anthony <anthony.tandoh@tanacom.io>
2022-09-14 23:03:50 -0700

    commit 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Prints first 50 fibonacci numbers start at 1 and  2
+* Return: 0 throw Success
+*/
+
+int main(void)
+{
+	int i = 0;
+	long int t1 = 1, t2 = 2, sum;
+
+	while (i < 50)
+	{
+		if (i != 49)
+			printf("%ld, ", t1);
+		else
+			printf("%ld\n", t1);
+		sum = t1 + t2;
+		t1 = t2;
+		t2 = sum;
+		++i;
+	}
+	return (0);
+}
