17e609c70c3d386ba2f736a24d17faad09c81de0
Anthony <anthony.tandoh@tanacom.io>
2022-09-16 14:19:58 -0700

    commit 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+* @a: first parmeter
+* @b: second parameter
+* Return: the result of the operation
+*/
+int mul(int a, int b)
+{
+	int total;
+
+	total = a * b;
+
+	return (total);
+}
