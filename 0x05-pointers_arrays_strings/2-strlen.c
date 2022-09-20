f2fbb04e07a99f5583c956bd6c0d6e6dbad9adab
Anthony <anthony.tandoh@tanacom.io>
2022-09-20 09:10:40 -0700

    commit 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - return the lenght of a string
+*@s: string pointer
+*Return: Lenght of the string
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
