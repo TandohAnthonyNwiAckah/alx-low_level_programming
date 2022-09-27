4ebbd6721ce49f94399003ab4de015046ad405e6
Anthony <anthony.tandoh@tanacom.io>
2022-09-27 14:51:34 -0700

    commit 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*set_string - sets a value of a pointer to a character
+*@s: parameter double pointer
+*@to:parameter single pointer
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
