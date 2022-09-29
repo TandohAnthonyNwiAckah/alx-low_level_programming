854018cf7c23fba64b6b5f1fd40b2707cc57bc05
Anthony <anthony.tandoh@tanacom.io>
2022-09-29 14:16:47 -0700

    commit 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,20 +10,20 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_my_sqrt(0, n));
+	return (_sqrt(0, n));
 }
 
 /**
-* _my_sqrt - return the square root of a number
+* _sqrt - return the square root of a number
 * @a: parameter a
 * @b: parameter b
 * Return: the square root of a
 */
-int _my_sqrt(int a, int b)
+int _sqrt(int a, int b)
 {
 	if (a > b / 2)
 		return (-1);
 	else if (a * a == b)
 		return (a);
-	return (_my_sqrt(a + 1, b));
+	return (_sqrt(a + 1, b));
 }
