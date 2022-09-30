813767aadf719fc5d9a25f41cc6425becb5d50cb
Anthony <anthony.tandoh@tanacom.io>
2022-09-30 07:47:27 -0700

    commit 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the minimum number of coins for an amount of money
+* @argc: The size of the argv array
+* @argv: An array of size argc
+* Return: least number of coins, 0 if negative amount, 1 if amount not given
+*/
+
+int main(int argc, char *argv[])
+{
+	int n, coins = 0;
+
+	/* validate input */
+	if (argc != 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/* convert string to int */
+	n = atoi(argv[1]);
+	coins += n / 25;
+
+	n = n % 25;
+	coins += n / 10;
+
+	n = n % 10;
+	coins += n / 5;
+
+	n = n % 5;
+	coins += n / 2;
+
+	n = n % 2;
+	coins += n / 1;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
