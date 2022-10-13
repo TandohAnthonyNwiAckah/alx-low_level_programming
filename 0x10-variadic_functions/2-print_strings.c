e439ee4eba80f2cc93577b49068dc6386aa768c4
Anthony <anthony.tandoh@tanacom.io>
2022-10-13 14:32:55 -0700

    commit 3-print_all.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,25 +1,34 @@
 #include "variadic_functions.h"
+
 /**
-* print_strings - prints strings
-* @separator: string to be printed between numbers
-* @n: number of arguments
+* print_strings - function that prints strings
+* @separator: separate strings
+* @n: number of strings passed
+* Return: nothing
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
 	va_list ap;
+	unsigned int i;
+	char *res;
 
-	if (separator == NULL)
-		return;
 	va_start(ap, n);
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; ++i)
 	{
-		if (va_arg(ap, char *) == NULL)
-			va_arg(ap, char *) = "nil";
-		printf("%d", va_arg(ap, char *));
-		if (i != (n - 1))
+		res = va_arg(ap, char*);
+		if (res != NULL)
+		{
+			printf("%s", res);
+		}
+		else
+		{
+			printf("(nill)");
+		}
+
+		if (separator && i < n - 1)
+		{
 			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(ap);
